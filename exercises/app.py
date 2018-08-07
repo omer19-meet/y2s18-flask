2af5b920a224352ee04d97a1a4b6b157862d8ee6
omer19-meet <omer19@meet.mit.edu>
2018-08-07 09:36:45 -0400

    complicated

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+    # return "ping-pong"
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+render_template(index.html)
