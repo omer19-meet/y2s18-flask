64ed5e693cd85cc8a486be6e53f74dbee779f813
omer19-meet <omer19@meet.mit.edu>
2018-08-08 07:13:54 -0400

    finish

--- exercises/app.py
+++ exercises/app.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     # return "ping-pong"
-    return render_template("index.html")
+    games = [ "Jungel-Speed", "Katan", "Chest", "Damka", "Cheat", "Cambio", "Shesh-Besh"]
+    return render_template("index.html",
+    	likes_same_sport = True,
+    	games=games)
+
 
 
 if __name__ == '__main__':
    app.run(debug = True)
+
 
 render_template(index.html)
